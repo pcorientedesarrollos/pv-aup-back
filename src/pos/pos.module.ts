14e018dfc552fe58a51b1ac365f5aba9264b7ddb
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-09-17 13:58:32 -0600

    fix(pos): add PosGastoCategoria to pos module forFeature

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -43,6 +43,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       PosUsuario,
       PosCorteCaja,
       PosGasto,
+      PosGastoCategoria,
       PosSucursal,
       PosVenta,
       PosVentaDetalle,
