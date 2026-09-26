a796fafe0ca096c856e621f669ddce75ebe99e57
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-09-26 11:40:21 -0600

    eliminacion de archivos .js

--- mod74.js
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let ts = fs.readFileSync('src/pos/pos.service.ts', 'utf8');
-
-ts = ts.replace('const partes = [];', 'const partes: string[] = [];');
-fs.writeFileSync('src/pos/pos.service.ts', ts, 'utf8');
-console.log('Fixed typescript strict array typing.');
